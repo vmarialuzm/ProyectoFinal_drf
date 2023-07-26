02fa9d68c47c9f4b29ef27c586ed2760823748a1
vmarialuzm <vmarialuzm@gmail.com>
2023-07-26 10:49:28 -0500

    añadí documentación con swagger

--- apipagos/urls.py
+++ apipagos/urls.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info (
+        title="Api de Pagos",
+        default_version="v1",
+        description="API de lista de pagos",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="vmarialuzm@gmail.com"),
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,
+    permission_classes=[AllowAny]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include("usuarios.urls")),
     path('', include("pagos.urls")),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
