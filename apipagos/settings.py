a66bc30b7a6d6512277c9e955b62b15428e31cc5
vmarialuzm <vmarialuzm@gmail.com>
2023-07-27 15:54:59 -0500

    error 500

--- apipagos/settings.py
+++ apipagos/settings.py
@@ -93,7 +93,10 @@ WSGI_APPLICATION = 'apipagos.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
