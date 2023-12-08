954f15230756631c4f888a7dc6c61a73e61d221f
vmarialuz <vmarialuzm@gmail.com>
2023-12-08 16:19:48 -0500

    fix: modify PaymentUserSerializer

--- versioned_pagos/v2/serializers.py
+++ versioned_pagos/v2/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from pagos.models import Services, PaymentUser, ExpiredPayments
 
 class ServicesSerializer(ModelSerializer):
@@ -7,9 +7,17 @@ class ServicesSerializer(ModelSerializer):
         fields = "__all__"
 
 class PaymentUserSerializer(ModelSerializer):
+    service_id = PrimaryKeyRelatedField(queryset = Services.objects.all())
     class Meta:
         model = PaymentUser
         fields = "__all__"
+    
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        if self.context.get('request').method in ['GET']:
+            service_id = ServicesSerializer(instance.service_id).data
+            ret['service_id'] = service_id
+        return ret
 
 class ExpiredPaymentsSerializer(ModelSerializer):
     class Meta:
